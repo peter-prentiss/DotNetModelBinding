ca974b76911b018041cb301aaab7572319c4b0e5
Peter <peter.prentiss@gmail.com>
2018-10-31 20:37:40 -0500

    Update HomeController

--- DotNetModelBinding/Controllers/HomeController.cs
+++ DotNetModelBinding/Controllers/HomeController.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using Microsoft.AspNetCore.Mvc;
+using DotNetModelBinding.Models;
 
 namespace DotNetModelBinding.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private IRepository repository;
+        public HomeController(IRepository repo)
         {
-            return View();
+            repository = repo;
         }
+        public ViewResult Index(int id) => View(repository[id]);
     }
 }
